e6b91db23034fec7462440468e9d60a1155e3f87
kvytautas <7vytautas7@gmail.com>
2026-02-18 11:19:31 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+
+nums = [1,55,2,3,4,5,6,7,8,9]
+# print(nums)
+# print(nums[0])
+# print(len(nums))
+#
+# nums.append(14)
+# print(nums)
+#
+# nums.sort()
+# print(nums)
+# nums.reverse()
+# print(nums)
+# nums.insert(2,30)
+# print(nums)
+#
+# nums.remove(9)
+# print(nums)
+# nums[8] = 8
+# print(nums)
+# print(nums.index(30))
+# nums[nums.index(2)] = 15
+# print(nums)
+#
+# #text arrays
+#
+# belekas = [1,True, "taip"]
+#
+# arr2d = [
+#        # 0 1 2 3
+#         [1,2,3,16], # 0
+#         [1,3,5,4], # 1
+#         [1,5,10,8] # 2
+# ]
+# print(arr2d)
+# print(arr2d[1][3])
+
+my_numbers = [1,2,3,4,5,6,7,8,9,10]
+print(my_numbers)
+print(my_numbers[6])
