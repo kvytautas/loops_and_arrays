dd0191be58764c352f0a9071abc7310d7556b515
kvytautas <7vytautas7@gmail.com>
2026-02-19 11:45:23 +0200

    Sunkesni, bet ne raudoni 20260219

--- sunkesni.py
+++ sunkesni.py
@@ -164,3 +164,6 @@ for i in range(vinys):
         kalta += 1
         print(f' smugis: {smugis} mm  pataikymas: {pataikymas}*100%')
     print(f' Vinis nr {i + 1}  įkalta per {kalta} kalimus')
+
+#9.	Sugeneruokite stringą, kurį sudarytų 50 atsitiktinių skaičių nuo 1 iki 200, atskirtų tarpais. Skaičiai turi būti unikalūs (t.y. nesikartoti). Sugeneruokite antrą stringą, pasinaudodami pirmu, palikdami jame tik pirminius skaičius (t.y tokius, kurie dalinasi be liekanos tik iš 1 ir patys savęs). Skaičius stringe sudėliokite didėjimo tvarka, nuo mažiausio iki didžiausio. (reikės split() funkcijos ir masyvų.)
+print("-------- 9 --------")
