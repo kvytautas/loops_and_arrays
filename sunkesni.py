39d9e7be4e0f3c2249b54216cff9685c9fdda3b9
kvytautas <7vytautas7@gmail.com>
2026-02-19 13:32:13 +0200

    Sunkesni išskyrus 7.rombą 20260219

--- sunkesni.py
+++ sunkesni.py
@@ -108,62 +108,81 @@ import random
 #         inarow += 1
 
 #Kazys ir Petras žaidžia šaškėm. Petras surenka taškų kiekį nuo 10 iki 20, Kazys surenka taškų kiekį nuo 5 iki 25. Vienoje eilutėje išvesti žaidėjų vardus su taškų kiekiu ir “Partiją laimėjo: laimėtojo vardas”. Taškų kiekį generuokite funkcija random.randint(x,x). Žaidimą laimi tas, kas greičiau surenka 222 taškus. Partijas kartoti tol, kol kažkuris žaidėjas pirmas surenka 222 arba daugiau taškų.
-print("-------- 6 --------")
+# print("-------- 6 --------")
 
-# kazys = random.randint(10, 20)
-# petras = random.randint(5, 25)
+# # kazys = random.randint(10, 20)
+# # petras = random.randint(5, 25)
+# #
+# # print(f' Kazys {kazys} "vs" Petras {petras} ')
 #
-# print(f' Kazys {kazys} "vs" Petras {petras} ')
-
-petras_total = 0
-kazys_total = 0
-
-while petras_total <= 222 and kazys_total <= 222:
-    petras = random.randint(10, 20)
-    kazys = random.randint(5, 25)
-    petras_total += petras
-    kazys_total += kazys
-    print(f' Petras {petras_total}  "vs"  Kazys {kazys_total}')
-
-if petras_total > kazys_total:
-    print(f'Partiją laimėjo: Petras {petras_total}  "vs"  Kazys {kazys_total}')
-elif petras_total == kazys_total:
-    print(f'Lygiosios: Petras {petras_total}  "vs"  Kazys {kazys_total}')
-else:
-    print(f'Partiją laimėjo: Kazys {kazys_total}  "vs" Petras {petras_total} ')
+# petras_total = 0
+# kazys_total = 0
+#
+# while petras_total <= 222 and kazys_total <= 222:
+#     petras = random.randint(10, 20)
+#     kazys = random.randint(5, 25)
+#     petras_total += petras
+#     kazys_total += kazys
+#     print(f' Petras {petras_total}  "vs"  Kazys {kazys_total}')
+#
+# if petras_total > kazys_total:
+#     print(f'Partiją laimėjo: Petras {petras_total}  "vs"  Kazys {kazys_total}')
+# elif petras_total == kazys_total:
+#     print(f'Lygiosios: Petras {petras_total}  "vs"  Kazys {kazys_total}')
+# else:
+#     print(f'Partiją laimėjo: Kazys {kazys_total}  "vs" Petras {petras_total} ')
 
 #Sumodeliuokite vinies kalimą. Įkalimo gylį sumodeliuokite pasinaudodami random.randint(x,x) funkcija. Vinies ilgis 8.5cm (pilnai sulenda į lentą).
 
 #“Įkalkite” 5 vinis mažais smūgiais. Vienas smūgis vinį įkala 5-20 mm. Suskaičiuokite kiek reikia smūgių.
-print("-------- 8a--------")
-
-vinys = 5
-
-for i in range(vinys):
-    ikalimas = 0
-    kalta = 0
-    while ikalimas <= 85:
-        smugis = random.randint(5, 20)
-        ikalimas += smugis
-        kalta += 1
-    print(f' Vinis nr {i + 1}  įkalta per {kalta} kalimus')
+# print("-------- 8a--------")
+#
+# vinys = 5
+#
+# for i in range(vinys):
+#     ikalimas = 0
+#     kalta = 0
+#     while ikalimas <= 85:
+#         smugis = random.randint(5, 20)
+#         ikalimas += smugis
+#         kalta += 1
+#     print(f' Vinis nr {i + 1}  įkalta per {kalta} kalimus')
 
 
 #““Įkalkite” 5 vinis dideliais smūgiais. Vienas smūgis vinį įkala 20-30 mm, bet yra 50% tikimybė (pasinaudokite random.randint(x,x) funkcija tikimybei sumodeliuoti), kad smūgis nepataikys į vinį. Suskaičiuokite kiek reikia smūgių.
-print("-------- 8b--------")
-
-vinys = 5
-
-for i in range(vinys):
-    ikalimas = 0
-    kalta = 0
-    while ikalimas <= 85:
-        smugis = random.randint(20, 30)
-        pataikymas = random.randint(0, 1)
-        ikalimas += smugis * pataikymas
-        kalta += 1
-        print(f' smugis: {smugis} mm  pataikymas: {pataikymas}*100%')
-    print(f' Vinis nr {i + 1}  įkalta per {kalta} kalimus')
+# print("-------- 8b--------")
+#
+# vinys = 5
+#
+# for i in range(vinys):
+#     ikalimas = 0
+#     kalta = 0
+#     while ikalimas <= 85:
+#         smugis = random.randint(20, 30)
+#         pataikymas = random.randint(0, 1)
+#         ikalimas += smugis * pataikymas
+#         kalta += 1
+#         print(f' smugis: {smugis} mm  pataikymas: {pataikymas}*100%')
+#     print(f' Vinis nr {i + 1}  įkalta per {kalta} kalimus')
 
 #9.	Sugeneruokite stringą, kurį sudarytų 50 atsitiktinių skaičių nuo 1 iki 200, atskirtų tarpais. Skaičiai turi būti unikalūs (t.y. nesikartoti). Sugeneruokite antrą stringą, pasinaudodami pirmu, palikdami jame tik pirminius skaičius (t.y tokius, kurie dalinasi be liekanos tik iš 1 ir patys savęs). Skaičius stringe sudėliokite didėjimo tvarka, nuo mažiausio iki didžiausio. (reikės split() funkcijos ir masyvų.)
+
 print("-------- 9 --------")
+
+numbers9 = random.sample(range(1, 201), 50)
+print(*numbers9)
+
+pirminiai = sorted([
+    i for i in numbers9
+    if i > 1 and all(i % d != 0 for d in range(2, int(i**0.5) + 1))
+])
+print("Pirminiai:", *pirminiai)
+
+
+ # int(i) for i in range(len(numbers9)):
+ #    if numbers9[i] < 2:
+ #        print(numbers9[i])
+ #    if i in range(2,int(len(numbers9)**0.5)+1):
+ #        if n % i == 0:
+ #            print(numbers9[i])
+
